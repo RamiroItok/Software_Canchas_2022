7ad60b43a61d9ce76eec12153b6d917ba7cc01ef
Ramiro Exequiel Itokazu <ramiro.itokazu@gmail.com>
2022-10-21 00:42:28 -0300

    Una sola instancia de la aplicación

--- Software_Canchas_2022/Program.cs
+++ Software_Canchas_2022/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -9,27 +13,62 @@ namespace Software_Canchas_2022
 {
     internal static class Program
     {
+        [DllImport("user32")]
+        public static extern bool SetForegroundWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        public static extern int FindWindow(string lpClassName, string lpWindowName);
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            bool instanceCountOne = false;
 
-            // Inyecciones de dependencias
-            BLL.Usuario usuario = new BLL.Usuario();
-            BLL.Cancha cancha = new BLL.Cancha();
-            BLL.Cliente cliente = new BLL.Cliente();
-            BLL.Reserva reserva = new BLL.Reserva();
-            BLL.Bitacora bitacora = new BLL.Bitacora();
-            BLL.ControlReserva controlReserva = new BLL.ControlReserva();
-            BLL.Backup backup = new BLL.Backup();
-            BLL.Observer.Idioma traductor = new BLL.Observer.Idioma();
-            BLL.Composite.Permiso permiso = new BLL.Composite.Permiso();
+            using (Mutex mutex = new Mutex(true, Process.GetCurrentProcess().ProcessName, out instanceCountOne))
+            {
+                if (instanceCountOne)
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new Login(permiso, traductor, cancha, cliente, usuario, reserva, bitacora, backup, controlReserva));
+                    // Inyecciones de dependencias
+                    BLL.Usuario usuario = new BLL.Usuario();
+                    BLL.Cancha cancha = new BLL.Cancha();
+                    BLL.Cliente cliente = new BLL.Cliente();
+                    BLL.Reserva reserva = new BLL.Reserva();
+                    BLL.Bitacora bitacora = new BLL.Bitacora();
+                    BLL.ControlReserva controlReserva = new BLL.ControlReserva();
+                    BLL.Backup backup = new BLL.Backup();
+                    BLL.Observer.Idioma traductor = new BLL.Observer.Idioma();
+                    BLL.Composite.Permiso permiso = new BLL.Composite.Permiso();
+
+                    Application.Run(new Login(permiso, traductor, cancha, cliente, usuario, reserva, bitacora, backup, controlReserva));
+                    mutex.ReleaseMutex();
+                }
+                else
+                {
+                    MessageBox.Show("La aplicación ya se encuentra en uso.");
+                    var processes = Process.GetProcessesByName("Software_Canchas_2022");
+                    foreach (var process in processes.Where(p => p.MainWindowHandle != IntPtr.Zero))
+                    {
+                        BringWindowToTop(process.MainWindowTitle);
+                    }
+                    Application.Exit();
+                }
+            }
+        }
+
+        private static bool BringWindowToTop(string windowName)
+        {
+            int hWnd = FindWindow(null, windowName);
+            if (hWnd != 0)
+            {
+                return SetForegroundWindow((IntPtr)hWnd);
+            }
+            return false;
         }
     }
 }
